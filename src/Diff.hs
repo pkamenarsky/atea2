d0a9cd2d3bd46705a8abfd648e084d1dfb72aa12
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-03-16 18:14:41 +0100

    Diff with moves WIP

---
+++ src/Diff.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE TupleSections #-}
+
+module Diff where
+
+import           Control.Arrow
+
+import           Data.Algorithm.Diff
+import           Data.Function
+
+diff :: ([a] -> Int) -> (a -> a -> Bool) -> [[a]] -> [[a]] -> [Diff a]
+diff hash cmp old new = go hashdiff
+  where
+    words :: [a] -> [[a]]
+    words = undefined
+
+    hashOld   = map (id &&& hash) old
+    hashNew   = map (id &&& hash) new
+    hashdiff  = getGroupedDiffBy ((==) `on` snd) hashOld hashNew
+
+    go [] = []
+    go (First c:Second c':cs) = linediff c c' ++ go cs
+    go (Second c:First c':cs) = linediff c' c ++ go cs
+    go (Both c c':cs) = concat (zipWith (\x y -> zipWith Both (fst x) (fst y)) c c') ++ go cs
+    go (First c:cs) = concatMap (map First . fst) c ++ go cs
+    go (Second c:cs) = concatMap (map Second . fst) c ++ go cs
+
+    linediff ls [] = concatMap (map First . fst) ls
+    linediff [] rs = concatMap (map Second . fst) rs
+    linediff lold@(l:ls) lnew@(l':ls') = getDiffBy cmp (fst l) (fst l') ++ linediff ls ls'
+      where
+        -- TODO: matach old and new lines by shortest edit distance
+        -- if edit distance > 75%, genereate deletes
+        -- if no matching old line, generate inserts
+        worddiff =
+          [ diff hash cmp (words lnOld) (words lnNew)
+          | [lnNew, lnOld] <- sequence [map fst ls', map fst ls]
+          ]
